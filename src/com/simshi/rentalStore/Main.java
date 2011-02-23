5619d5f8640dfb53ba275eecbc24d5d72c609ae2
simshi <simonshi@gmail.com>
2011-02-23 10:39:47 +0800

    add basic case, in main...

--- src/com/simshi/rentalStore/Main.java
+++ src/com/simshi/rentalStore/Main.java
@@ -7,10 +7,13 @@ public class Main {
 		Movie m1 = new Movie("star trek 1", Movie.REGULAR);
 		Movie m2 = new Movie("star trek 11", Movie.NEW_RELEASE);
 		Movie m3 = new Movie("xyy", Movie.CHILDRENS);
-		Movie m3 = new Movie("Transferman 3", Movie.NEW_RELEASE);
+		Movie m4 = new Movie("Transferman 3", Movie.NEW_RELEASE);
 		
 		Customer c = new Customer("bb");
 		c.addRental(new Rental(m1, 3));
+		c.addRental(new Rental(m2, 4));
+		c.addRental(new Rental(m3, 5));
+		c.addRental(new Rental(m4, 6));
 		System.out.print(c.statement());
 	}
 
