787533b75451cf77c9fb466279578249c798f3a7
simshi <simonshi@gmail.com>
2011-02-27 14:40:15 +0800

    replace temp total with query.

--- src/com/simshi/rentalStore/Customer.java
+++ src/com/simshi/rentalStore/Customer.java
@@ -20,7 +20,6 @@ class Customer {
 	}
 
 	public String statement() {
-		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		Enumeration<Rental> rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
@@ -31,12 +30,21 @@ class Customer {
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t"
 					+ String.valueOf(each.getCharge()) + "\n";
-			totalAmount += each.getCharge();
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
+		return result;
+	}
+
+	private double getTotalCharge() {
+		double result = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		while (rentals.hasMoreElements()) {
+			Rental each = rentals.nextElement();
+			result += each.getCharge();
+		}
 		return result;
 	}
 }
