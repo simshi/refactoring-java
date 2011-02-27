b93ad4dfd673aa3994dd3cc8787c6df781953407
simshi <simonshi@gmail.com>
2011-02-27 14:35:37 +0800

    extract and move method getFrequentRenterPoints.

--- src/com/simshi/rentalStore/Rental.java
+++ src/com/simshi/rentalStore/Rental.java
@@ -35,4 +35,12 @@ class Rental {
 		}
 		return result;
 	}
+
+	int getFrequentRenterPoints() {
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE)
+				&& getDaysRented() > 1)
+			return 2;
+		else
+			return 1;
+	}
 }
